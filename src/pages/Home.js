0350e54fa62243f0bba69d3f834dd2a1f79d0902
aluno <aluno@LAB.ETEC187>
2022-09-05 20:53:54 -0300

    Adicionando o botão para salvar um filme

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,22 @@
 import react from "react";
 
 import {
-    View, 
-    Text, 
-    TextInput, 
-    TouchableOpacity, 
-    StyleSheet
-} from 'react-native';
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
-export default function Home (){
-    return(
-        <View>
-            <Text>Meus Filmes Favoritos</Text>
-            <Text>Olá Maiara, boa noite!</Text>
-        </View>
-    );
+export default function Home() {
+  return (
+    <View>
+      <Text>Meus Filmes Favoritos</Text>
+      <Text>Olá Maiara, boa noite!</Text>
+
+      <TouchableOpacity>
+        <Text>Adicionar</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
-
