9eb7dc91f917aed36b892eb2c24feed7166c6c5c
aluno <aluno@LAB.ETEC187>
2022-09-05 22:25:10 -0300

    Finalizando a interface da tela Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,13 +10,55 @@ import {
 
 export default function Home() {
   return (
-    <View>
-      <Text>Meus Filmes Favoritos</Text>
-      <Text>Olá Maiara, boa noite!</Text>
-
-      <TouchableOpacity>
-        <Text>Adicionar</Text>
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Meus Filmes Favoritos</Text>
+      <Text style={styles.subtitulo}>Olá Maiara, boa noite!</Text>
+      <TextInput style={styles.campo} placeholder="Nome do filme" />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Adicionar</Text>
       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#30478C",
+    paddingVertical: 70,
+    paddingHorizontal: 20,
+  },
+  titulo: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#FFD700",
+  },
+  subtitulo: {
+    color: "#FFF",
+    fontSize: 15,
+  },
+  campo: {
+    backgroundColor: "#1F1E25",
+    color: "#FFF",
+    fontSize: 18,
+    marginTop: 30,
+    borderRadius: 7,
+    padding: 15,
+  },
+  greetings: {
+    color: "#FFF",
+  },
+  button: {
+    backgroundColor: "#A370F7",
+    padding: 15,
+    borderRadius: 7,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+});
